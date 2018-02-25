b283ec91d1284e79f1d7980211feed7a679c85b5
yaronmiro <yaron.miro@gmail.com>
2018-02-26 00:07:06 +0200

    Adding more logic

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { EditorStatus } from './status';
 import { Product } from './../types/product';
 import { ProductService } from './../services/product/product.service';
+
 
 
 @Component({
@@ -11,17 +14,27 @@ import { ProductService } from './../services/product/product.service';
 })
 export class ProductsComponent implements OnInit {
 
-  selectedProductId: number | null = null;
-
   products: Product[];
 
   newProduct: Product = new Product();
 
-  constructor(private productService: ProductService) { }
+  editorStatus: EditorStatus = EditorStatus.Nothing;
+
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
     // Get all exisiting products.
     this.products = this.productService.products;
+  }
+
+  // Set the status when adding/editing a product.
+  setStatusToNewOrEdit() {
+    this.editorStatus = EditorStatus.NewOrEdit;
+  }
+
+  // Check if the given status is new or edit status.
+  isStatusNewOrEdit() {
+    return this.editorStatus === EditorStatus.NewOrEdit;
   }
 
   // Toggle the product "checked" status.
---
+++ src/app/products/status.ts
@@ -0,0 +1,4 @@
+export enum EditorStatus {
+    NewOrEdit,
+    Nothing,
+  }

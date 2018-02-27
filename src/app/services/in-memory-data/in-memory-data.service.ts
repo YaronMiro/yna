9f088770be9b0b93f62b5e30c40fb7d7d316694a
yaronmiro <yaron.miro@gmail.com>
2018-02-27 12:01:00 +0200

    Adding comment

--- src/app/services/in-memory-data/in-memory-data.service.ts
+++ src/app/services/in-memory-data/in-memory-data.service.ts
@@ -4,14 +4,17 @@ import { Product } from './../../types/product';
 
 export class InMemoryDataService implements InMemoryDbService {
 
-  // Default "genId" method craches request when their are no enteries in the DB, when not supplying
-  // a given ID with the Product.
-  // Return the max number of ids plus one (+1) if they exists or the initial number (1) if it does not.
+  // Overriding the default "genId" method,
+  // This method does not add a dynamic ID on request
+  // when their are no entities in the DB.
+  // Return the max number of ids plus one (+1)
+  // if they exists or the initial number (1) if it does not.
   genId(products: Product[]): number {
     return products.length > 0 ? Math.max(...products.map(product => product.id)) + 1 : 1;
   }
 
   createDb() {
+    // The initial DB entitie.
     const products = [
       new Product({id: 1, title: 'milk', isChecked: true, quantity: '2'}),
       new Product({id: 2, title: 'bread'}),

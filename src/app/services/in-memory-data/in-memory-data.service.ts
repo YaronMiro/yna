8ba88ccd0ed10be1c4e8388f36527e9d33ee7aec
yaronmiro <yaron.miro@gmail.com>
2018-02-27 11:42:47 +0200

    Fix DB !!!!!!! yes

--- src/app/services/in-memory-data/in-memory-data.service.ts
+++ src/app/services/in-memory-data/in-memory-data.service.ts
@@ -3,11 +3,21 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Product } from './../../types/product';
 
 export class InMemoryDataService implements InMemoryDbService {
+
+  // Default "genId" method craches request when their are no enteries in the DB, when not supplying
+  // a given ID with the Product.
+  // Return the max number of ids plus one (+1) if they exists or the initial number (1) if it does not.
+  genId(products: Product[]): number {
+    return products.length > 0 ? Math.max(...products.map(product => product.id)) + 1 : 1;
+  }
+
   createDb() {
     const products = [
       new Product({id: 1, title: 'milk', isChecked: true, quantity: '2'}),
-      new Product({id: 2, title: 'bread'})
+      new Product({id: 2, title: 'bread'}),
+      new Product({id: 3, title: 'Cheerios'})
     ];
+
     return {products};
   }
 }

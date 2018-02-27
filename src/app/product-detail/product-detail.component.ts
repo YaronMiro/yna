438c6830c8e676483d33649e02f447a6e06f53a9
yaronmiro <yaron.miro@gmail.com>
2018-02-27 17:34:44 +0200

    Product titel debounce works

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { of } from 'rxjs/observable/of';
+
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
 import { Product } from './../types/product';
 import { ProductService } from './../services/product/product.service';
 
@@ -19,6 +25,9 @@ export class ProductDetailComponent implements OnInit {
     this.productService.update(this.product).subscribe();
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    
+
+  }
 
 }
